6ae3709e23438ac791437f345d4b66f88be81d42
Federico <f.gabrielvillalba@gmail.com>
2021-08-30 12:13:11 -0300

    model1

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,26 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight:  {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lifeSpan: {
+      type: DataTypes.STRING,
+    }
   });
 };
