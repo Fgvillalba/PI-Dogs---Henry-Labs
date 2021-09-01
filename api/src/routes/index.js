af903c3d9666676430b4d4d4b39388271d88cb60
Federico <f.gabrielvillalba@gmail.com>
2021-08-31 22:45:09 -0300

    falta el post

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -21,26 +21,25 @@ router.get('/dogs', async (req, res) => {
  
  const race =  req.query.name;
  const apiInfo = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
- const dataInfo = await Dog.findAll({
-  include: Temperament
+ const infoRes =  apiInfo.data.map(raza => {
+  return {
+      id: raza.id,
+      name: raza.name,
+      weight: raza.weight.metric,
+      temperament: raza.temperament,
+      image: raza.image.url,
+  }
  });
- const info = apiInfo.data.concat(dataInfo);
- const infoRes =  info.map(raza =>{
-    return {
-        id: raza.id,
-        name: raza.name,
-        weight: raza.weight.metric,
-        temperament: raza.temperament,
-        image: raza.image.url,
-    }
-  });
+ const dataInfo = await Dog.findAll( { attributes: ['id', 'name', 'weight', 'image'] });
+ const info = infoRes.concat(dataInfo);
+ 
   if(race){
-    const filterByName = infoRes.filter((raza)=> raza.name.toLocaleLowerCase().includes(race.toLocaleLowerCase()));
+    const filterByName = info.filter((raza)=> raza.name.toLocaleLowerCase().includes(race.toLocaleLowerCase()));
     filterByName.length?
     res.status(200).json(filterByName): res.status(404).send("No existe una raza que incluya name")
   }
    res.status(200)
-      .json(infoRes)
+      .json(info)
 });
 
 
@@ -58,9 +57,7 @@ router.get('/dogs/:idRaza', async (req, res) => {
         life_span: raza.life_span
     }
  });
- const dataInfo = await Dog.findAll({
-    include: Temperament
-   });
+ const dataInfo = await Dog.findAll({ attributes: ['id', 'name','height' ,'weight', 'image', 'life_span']});
  const info = infoRes.concat(dataInfo);
  const dog = info.find((r) => r.id == id);
  if(dog){
@@ -73,34 +70,36 @@ router.get('/dogs/:idRaza', async (req, res) => {
 
 
 router.get('/temperament', async function(req, res) {
+ const dataTemps = await Temperament.findAll();
+ if(dataTemps.length){
+ res.status(200).json(dataTemps)
+ } else {
  const apiInfo = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
- const array = [];
  apiInfo.data.forEach(element => {
-   const temp = element.temperament?.split(",");
+   const temp = element.temperament?.split(',');
    temp?.forEach(t => {
     Temperament.findOrCreate({
       where: {
-        name: t.replace(" ", "")
+        name: t.replace(' ', '')
       }
     })
    })
   });
- });
+  const dataTemps = await Temperament.findAll();
+  res.status(200).json(dataTemps);
+ }
+});
  
 
-
-
-
 router.post("/dog", async function(req, res) {
-    const {name, height, weight, life_span, image, temperaments} = req.body;
-    const dog = await Dog.create({
-        name, height, weight, life_span, image
-    })
-
-
-    // dog.setTemperaments(temperaments);
-    res.json(dog);
-
+ const { name, height, weight, life_span, image, temperaments } = req.body;
+ if(name && height && weight && life_span ){
+ const dog = await Dog.create({ name, height, weight, life_span, image })
+//  dog.setTemperaments(temperaments);
+ res.status(200).json(dog);
+ } else {
+   res.status(400).send("Faltan inputs");
+ }
 })
 
 
